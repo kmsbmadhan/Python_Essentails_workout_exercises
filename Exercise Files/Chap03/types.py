802b59542d618565d18f598aece8c734a479baed
kmsbmadhan <kmsbmadhan@gmail.com>
2020-04-07 19:34:35 +0100

    Worked out files

---
+++ Exercise Files/Chap03/types.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+###Print Second largest number in a list
+
+def Second_largest_number(lst):
+    largest_num = None
+    second_largest = None
+    for num in lst:
